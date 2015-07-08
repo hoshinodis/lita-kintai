1014461ec20d9fd826255462a0c8f3c62f95a8bd
hoshino <okonomisentai@gmail.com>
2015-07-08 11:56:49 +0900

    ーを｜にするように

--- lib/lita/handlers/kintai.rb
+++ lib/lita/handlers/kintai.rb
@@ -13,7 +13,7 @@ module Lita
       def tanzaku(response)
         negaigoto = response.args[0].chars
         tanzaku = "┏┷┓\n" + negaigoto.map{|item| "┃" + item + "┃\n"}.join + "┗━★"
-        response.reply(tanzaku)
+        response.reply(tanzaku.gsub("ー", "｜"))
       end
     end
 
