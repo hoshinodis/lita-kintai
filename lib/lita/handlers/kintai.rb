79c1d3acb76ea6e19d2418458bd2e8f64c38209f
hoshino <okonomisentai@gmail.com>
2015-07-10 13:13:49 +0900

    selfじゃなかった

--- lib/lita/handlers/kintai.rb
+++ lib/lita/handlers/kintai.rb
@@ -1,11 +1,11 @@
 module Lita
   module Handlers
     class Kintai < Handler
-      time = Time.now
 
       route(/しごはじ/i, :start_work) 
 
       def start_work(response)
+        time = Time.now
         start_reply = "#{response.user.name}さんが#{self.time.strftime('%H時%M分')}に出社しました"
         response.reply(start_reply)
       end
@@ -13,6 +13,7 @@ module Lita
       route(/しごとわた/i, :end_work)
 
       def end_work(response)
+        time = Time.now
         end_reply = "#{response.user.name}さんが#{self.time.strftime('%H時%M分')}に退社しました"
         response.reply(end_reply)
       end
