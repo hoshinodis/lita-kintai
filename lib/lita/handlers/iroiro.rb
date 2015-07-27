78e4eb126903f8df6db99ed4549a48dff491d824
hoshino <okonomisentai@gmail.com>
2015-07-27 13:28:56 +0900

    突然の死

--- lib/lita/handlers/iroiro.rb
+++ lib/lita/handlers/iroiro.rb
@@ -1,15 +1,25 @@
 module Lita
   module Handlers
     class Iroiro < Handler
-      route(/info response/i, :info_response)
 
+      route(/^突然の/i, :suddenly) 
+      def suddenly(response)
+        word = response.args[0]
+        suddenly_word = "＿"
+        (word.length).times{suddenly_word += "人"}
+        suddenly_word += "＿\n＞ 突然の#{word} ＜\n￣"
+        (word.length).times{suddenly_word += "Y^"}
+        suddenly_word += "￣"
+        response.reply(suddenly_word)
+      end
+
+      route(/info response/i, :info_response)
       def info_response(response)
         reply = "#{response}"
         response.reply(reply)
       end
 
       route(/info user/i, :info_user)
-
       def info_user(response)
         reply = "#{response.user.name}"
         response.reply(reply)
