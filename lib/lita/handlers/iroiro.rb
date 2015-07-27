746ec67a4e8e4c185ba323c64119c6032c42cdae
hoshino <okonomisentai@gmail.com>
2015-07-27 14:55:27 +0900

    iroiro確認

--- lib/lita/handlers/iroiro.rb
+++ lib/lita/handlers/iroiro.rb
@@ -5,17 +5,18 @@ module Lita
       route(/^突然の/i, :suddenly) 
       def suddenly(response)
         word = response.args.join
+        word = "死" if word.nil?
         suddenly_word = "＿"
-        (word.length + 2).times{suddenly_word += "人"}
-        suddenly_word += "＿\n＞ #{word} ＜\n￣"
-        (word.length + 2).times{suddenly_word += "Y^"}
+        (word.length + 5).times{suddenly_word += "人"}
+        suddenly_word += "＿\n＞ 突然の#{word} ＜\n￣"
+        (word.length + 5).times{suddenly_word += "Y^"}
         suddenly_word += "￣"
         response.reply(suddenly_word)
       end
 
       route(/info message/i, :info_message)
       def info_message(response)
-        reply = "#{response.message.source}\n#{response.message}"
+        reply = "#{response.message.source.private_message}\n#{response.reply}"
         response.reply(reply)
       end
 
