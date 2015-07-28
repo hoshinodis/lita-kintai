7bcd78d8a136dd75c9d8fe03bf258132fcad4b53
hoshino <okonomisentai@gmail.com>
2015-07-28 13:06:27 +0900

    わかる

--- lib/lita/handlers/iroiro.rb
+++ lib/lita/handlers/iroiro.rb
@@ -14,15 +14,15 @@ module Lita
         response.reply(suddenly_word)
       end
 
-      route(/ねー$|ねー。$/, :consent)
+      route(/ねー$|よー$|ね～$|な～$|なー$/, :consent)
       def consent(response)
-        reply_list = %w(わかる それな あっ わかるわ～ わかる、本質からわかる。わかってしまったのだ… そうだよね～ はいわかりました！ 直感でわかったけど、じっくり考えて深く理解した ん～！まさかそう思ってるひとがりったん以外にもいたとは！ かなりわかる わかった！一番わかった！ さわやかなわかり わかるという感じか 軽やかなわかり そう げにげに 自動的にわかった).freeze
+        reply_list = %w(わかる それな あっ わかるわ～ わかる、本質からわかる。わかってしまったのだ… そうだよね～ 直感でわかったけど、じっくり考えて深く理解した ん～！まさかそう思ってるひとがりったん以外にもいたとは！ かなりわかる わかった！一番わかった！ さわやかなわかり わかるという感じか 軽やかなわかり そう げにげに 自動的にわかった はいりったんわかるわかる わかる～わかるわ～～～ うんわかるよ、女の人ってそうだからすぐ論点ずれていくよね。勘弁して欲しいよね。 知らんがな そう それ言おうと思ってた わかる気がする うん それを求めてた).freeze
         response.reply(reply_list.sample)
       end
 
       route(/info message/i, :info_message)
       def info_message(response)
-        reply = "#{response.message.source.private_message}\n#{response.reply}"
+        reply = "#{response.matches[0]}\n#{response.matches[0][0]}"
         response.reply(reply)
       end
 
