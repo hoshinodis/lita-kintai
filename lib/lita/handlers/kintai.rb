0825cb70a13dfedfdc46247f3f12fd827dec03e8
hoshinodis <okonomisentai@gmail.com>
2017-02-08 17:23:39 +0900

    Update kintai.rb

--- lib/lita/handlers/kintai.rb
+++ lib/lita/handlers/kintai.rb
@@ -48,7 +48,7 @@ module Lita
           syussya_time = syussya_time - 60 if syussya_time >= (8 * 60)
           update_query = "update #{TABLE_NAME} set end_at = '#{datetime(time)}', syussya_time = #{syussya_time} where id = '#{response.user.id}' and end_at = '#{datetime(result['start_at'])}'"
           client.query(update_query) if result['start_at'] == result['end_at']
-          reply = "#{response.user.name}さんが#{time.strftime("%H時%M分")}に退勤しました"
+          reply = "#{response.user.name}さんが#{time.strftime("%H時%M分")}に退勤しました。更新する場合は `kintai update #{syussya_time} <退勤日時yyyy-MM-dd>` です。"
           return response.reply(reply)
         end
 
