bc2218e2afb07ecf6444c9d675be68952b804333
hoshinodis <okonomisentai@gmail.com>
2018-09-11 14:33:17 +0900

    Update iroiro.rb

--- lib/lita/handlers/iroiro.rb
+++ lib/lita/handlers/iroiro.rb
@@ -15,6 +15,35 @@ module Lita
         reply = "トゥットゥルー"
         response.reply_with_mention(reply)
       end
+
+      route(/こうぺん/, :koutei_panda)
+      def koutei_panda(response)
+        reply_list = %w(https://twitter.com/kosatsuneDQX/status/968482592977596417 https://twitter.com/kosatsuneDQX/status/970295845894594561 https://twitter.com/kosatsuneDQX/status/983677371466182657
+                        https://twitter.com/kosatsuneDQX/status/968844166896603136 https://twitter.com/kosatsuneDQX/status/970655586856521728 https://twitter.com/kosatsuneDQX/status/984068448366936066
+                        https://twitter.com/kosatsuneDQX/status/969183054739550208 https://twitter.com/kosatsuneDQX/status/971014195696123904 https://twitter.com/kosatsuneDQX/status/984454777990819841
+                        https://twitter.com/kosatsuneDQX/status/969195146192416768 https://twitter.com/kosatsuneDQX/status/971386458744373249 https://twitter.com/kosatsuneDQX/status/985135126827814913
+                        https://twitter.com/kosatsuneDQX/status/969538525791100928 https://twitter.com/kosatsuneDQX/status/971691968773287939 https://twitter.com/kosatsuneDQX/status/986943614709186561
+                        https://twitter.com/kosatsuneDQX/status/969942697552183296 https://twitter.com/kosatsuneDQX/status/971728285871689729 https://twitter.com/kosatsuneDQX/status/987698634824142849
+                        https://twitter.com/kosatsuneDQX/status/972108197187108865 https://twitter.com/kosatsuneDQX/status/972477537266708481 https://twitter.com/kosatsuneDQX/status/988414254351970306
+                        https://twitter.com/kosatsuneDQX/status/972834663730262018 https://twitter.com/kosatsuneDQX/status/973190486855794689 https://twitter.com/kosatsuneDQX/status/989526762135502848
+                        https://twitter.com/kosatsuneDQX/status/973573135248236545 https://twitter.com/kosatsuneDQX/status/973920562044006400 https://twitter.com/kosatsuneDQX/status/992413734835441664
+                        https://twitter.com/kosatsuneDQX/status/974283751499841536 https://twitter.com/kosatsuneDQX/status/974651682066870272 https://twitter.com/kosatsuneDQX/status/992761009575116800
+                        https://twitter.com/kosatsuneDQX/status/975025167930765312 https://twitter.com/kosatsuneDQX/status/975385750513446913 https://twitter.com/kosatsuneDQX/status/995307225018126336
+                        https://twitter.com/kosatsuneDQX/status/976118137065099264 https://twitter.com/kosatsuneDQX/status/976473277752262657 https://twitter.com/kosatsuneDQX/status/996035468478197767
+                        https://twitter.com/kosatsuneDQX/status/976830517533294592 https://twitter.com/kosatsuneDQX/status/977207569906200577 https://twitter.com/kosatsuneDQX/status/996771232996470784
+                        https://twitter.com/kosatsuneDQX/status/977886107240050689 https://twitter.com/kosatsuneDQX/status/978634308775591941 https://twitter.com/kosatsuneDQX/status/997487588020666374
+                        https://twitter.com/kosatsuneDQX/status/979374667063476224 https://twitter.com/kosatsuneDQX/status/979678244839374850 https://twitter.com/kosatsuneDQX/status/998197781972992000
+                        https://twitter.com/kosatsuneDQX/status/980371567090675712 https://twitter.com/kosatsuneDQX/status/981146153331785728 https://twitter.com/kosatsuneDQX/status/1000740304054439936
+                        https://twitter.com/kosatsuneDQX/status/981506725441236992 https://twitter.com/kosatsuneDQX/status/982274839959318528 https://twitter.com/kosatsuneDQX/status/1001556900352180224
+                        https://twitter.com/kosatsuneDQX/status/1002901793620164608 https://twitter.com/kosatsuneDQX/status/1004359545215279105 https://twitter.com/kosatsuneDQX/status/1005460500795150339
+                        https://twitter.com/kosatsuneDQX/status/1005789510431461377 https://twitter.com/kosatsuneDQX/status/1008734907110051841 https://twitter.com/kosatsuneDQX/status/1009458841707114496
+                        https://twitter.com/kosatsuneDQX/status/1014183177840091136 https://twitter.com/kosatsuneDQX/status/1015594000751812608 https://twitter.com/kosatsuneDQX/status/1018044511421792256
+                        https://twitter.com/kosatsuneDQX/status/1021795647274414081 https://twitter.com/kosatsuneDQX/status/1022081392262299648 https://twitter.com/kosatsuneDQX/status/1029399132190851074
+                        https://twitter.com/kosatsuneDQX/status/1030831984342315017 https://twitter.com/kosatsuneDQX/status/1031916833174052865 https://twitter.com/kosatsuneDQX/status/1034048319079608320
+                        https://twitter.com/kosatsuneDQX/status/1037097545657344000
+                     ).freeze
+        response.reply(reply_list.sample)
+      end
       
       route(/工数見積もり/, :estimate)
       def estimate(response)
